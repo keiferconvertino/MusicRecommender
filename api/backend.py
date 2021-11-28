7710ee2efff0291112550349c834d47d0a8e1bf9
Keifer Convertino <kjc6@rice.edu>
2021-11-28 13:11:24 -0500

    mess with static

--- api/backend.py
+++ api/backend.py
@@ -12,7 +12,7 @@ from model import recommend_listener
 columns = ['track_id', 'popularity', 'acousticness', 'danceability', 'liveness','loudness', 'speechiness', 'tempo', 'valence', 'genre', 'artist_name', 'track_name']
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(creds.client_id, creds.client_secret))
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='/build', static_url_path='/')
 
 @app.route('/')
 def index():
